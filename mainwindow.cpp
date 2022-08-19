8c3214ddb7041eab7c38fc8fbcf671f3445b231e
Alina Yezhkova <alina.yezhkova@stud.onu.edu.ua>
2022-08-19 21:26:19 +0300

    do sth with ui initialization

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,12 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_connectButton_clicked()
+{
+    std::string username = ui->myUsernameLabel->text().toStdString();
+    std::string address = ui->addressLabel->text().toStdString();
+    std::string port = ui->portLabel->text().toStdString();
+
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,18 +2,22 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "SessionWrapperDelegate.hpp"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
+class MainWindow : public QMainWindow, public SessionWrapperAbstract
 {
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_connectButton_clicked();
 
 private:
     Ui::MainWindow *ui;
